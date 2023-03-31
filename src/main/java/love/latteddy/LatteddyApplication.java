81d1502a36ff1cadc30030f817533149c2b102e5
lattedy <stary7stars@naver.com>
2023-04-01 01:36:04 +0900

    Init

---
+++ src/main/java/love/latteddy/LatteddyApplication.java
@@ -0,0 +1,13 @@
+package love.latteddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatteddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatteddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/love/latteddy/LatteddyApplicationTests.java
@@ -0,0 +1,13 @@
+package love.latteddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatteddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
